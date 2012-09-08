af6ecfd66aec49e1e14ff9c456c9a984862c0ef0
iampersistent <develop@zestic.com>
2012-09-08 13:44:26 -0700

    add composer and travis-ci

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,19 +1,10 @@
 <?php
 
-require_once $_SERVER['SYMFONY'].'/Symfony/Component/ClassLoader/UniversalClassLoader.php';
-
-$loader = new Symfony\Component\ClassLoader\UniversalClassLoader();
-$loader->registerNamespace('Symfony\\Tests', $_SERVER['SYMFONY_TESTS']);
-$loader->registerNamespace('Symfony', $_SERVER['SYMFONY']);
-$loader->registerNamespace('Doctrine\\ODM\\MongoDB', $_SERVER['DOCTRINE_MONGODB_ODM']);
-$loader->registerNamespace('Doctrine\\MongoDB', $_SERVER['DOCTRINE_MONGODB']);
-$loader->registerNamespace('Doctrine\\Common', $_SERVER['DOCTRINE_COMMON']);
-$loader->register();
-
-spl_autoload_register(function($class)
-{
-    if (0 === strpos($class, 'Symfony\\Bundle\\DoctrineMongoDBBundle\\') &&
-        file_exists($file = __DIR__.'/../'.implode('/', array_slice(explode('\\', $class), 3)).'.php')) {
-        require_once $file;
-    }
-});
+if (!@include __DIR__ . '/../vendor/autoload.php') {
+    die(<<<'EOT'
+You must set up the project dependencies, run the following commands:
+wget http://getcomposer.org/composer.phar
+php composer.phar install
+EOT
+    );
+}
